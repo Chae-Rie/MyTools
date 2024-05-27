e93a61f8bc4d92cea45820be83cbfb551fa52153
yoocherry <yoospam@protonmail.com>
2024-05-27 22:42:16 +0200

    Anpassen der Projektstruktur in Vorbereitung auf die Bereitsstellung eines eigene CMake-Pakets

---
+++ include/MyTools/mytools.h
@@ -0,0 +1,9 @@
+#ifndef MYTOOLS_H
+#define MYTOOLS_H
+
+namespace MyTools
+{
+    void say_hello();
+};
+
+#endif // MYTOOLS_H
---
+++ src/mytools.cpp
@@ -0,0 +1,9 @@
+#include "../include/MyTools/mytools.h"
+
+#include <iostream>
+
+namespace MyTools {
+void say_hello() {
+    std::cout << "Hello from MyTools!" << std::endl;
+}
+}
