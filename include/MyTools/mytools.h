9f4b2b4da94dd1aacdbc3e645e6efaae3f1a115e
yoocherry <yoospam@protonmail.com>
2024-06-04 23:10:38 +0200

    Aufräumen, sodass main branch clean wird

--- include/MyTools/mytools.h
+++
@@ -1,9 +0,0 @@
-#ifndef MYTOOLS_H
-#define MYTOOLS_H
-
-namespace MyTools
-{
-    void say_hello();
-};
-
-#endif // MYTOOLS_H
--- src/mytools.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/MyTools/mytools.h"
-
-#include <iostream>
-
-namespace MyTools {
-void say_hello() {
-    std::cout << "Hello from MyTools!" << std::endl;
-}
-}
