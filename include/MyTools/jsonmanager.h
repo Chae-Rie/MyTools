781f2cebd51567bc120e961f91a86115ce03dd5a
yoocherry <yoospam@protonmail.com>
2024-09-24 22:59:51 +0200

    Add new JSON handling functions: GetObjectFromArray, GetStringValue
    Refactored code to introduce two new functions for improved JSON handling: `GetObjectFromArray` and `GetStringValue`. These functions allow extraction of JSON objects from arrays and retrieval of string values with default options, respectively. Enhanced the `ReadFile` function with error handling and resource management improvements.

--- include/MyTools/jsonmanager.h
+++ include/MyTools/jsonmanager.h
@@ -7,14 +7,13 @@
 #define JSONMANAGER_H
 
 #include <nlohmann/json.hpp>
-
+#include <string>
 using json = nlohmann::json;
 
-namespace MyTools {
-    namespace JsonManager {
-        // code
-        json ReadFile(const std::string fileName);
-
+    namespace MyTools::JsonManager {
+        json ReadFile(const std::string& fileName);
+        json GetObjectFromArray(const json& config, const std::string& key, int index = 0);
+        std::string GetStringValue(const json& jsonObj, const std::string& key, const std::string& defaultValue = "");
     }
-}
+
 #endif //JSONMANAGER_H
--- src/jsonmanager.cpp
+++ src/jsonmanager.cpp
@@ -2,11 +2,42 @@
 // Created by Yoo Cherry on 24.09.24.
 //
 #include "../include/MyTools/jsonmanager.h"
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
-namespace MyTools {
-    namespace JsonManager {
-        json ReadFile(const std::string fileName) {
-
+namespace MyTools::JsonManager {
+    // Reads a file from the given path and uses the returned stream to pollute a json-object with it
+    json ReadFile(const std::string &fileName) {
+        std::ifstream fileStream(fileName);
+        if (!fileStream.is_open()) {
+            throw std::runtime_error("Could not open file " + fileName);
         }
+
+        json configData;
+        try {
+            fileStream >> configData;
+        } catch (const json::parse_error &ex) {
+            std::cerr << "Parse error at byte " << ex.byte << ": " << ex.what() << std::endl;
+            throw;
+        }
+        // explicitly close the file stream
+        fileStream.close();
+
+        return configData;
+    }
+
+    // Returns a smaller json object from an array with an index
+    json GetObjectFromArray(const json &config, const std::string &key, int index) {
+        if (config.contains(key) && config[key].is_array() && config[key].size() > index) {
+            return config[key][index];
+        }
+        std::cerr << "Error: '" << key << "' is missing or not an array." << std::endl;
+        return json();
+    }
+
+    // Returns a simple value under the given key, defaultvalue is nothing, to make it expectable
+    std::string GetStringValue(const json &jsonObj, const std::string &key, const std::string &defaultValue ){
+        return jsonObj.value(key, defaultValue);
     }
 }
